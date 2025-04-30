d842fd867d3ee9fe432af6bad5e0e6f36022ab3e
yusufalp <alpyusuf007@gmail.com>
2025-04-30 16:41:26 -0700

    build a form for filters

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { Suspense } from "react";
+
 import UserList from "./UserList";
 
 function App() {
   return (
     <>
       <h1>Users</h1>
+
+      <form>
+        <label htmlFor="results">Results</label>
+        <input type="number" name="results" id="results" value="200" />
+        <label htmlFor="nat">Nationality</label>
+        <select name="nat" id="nat" defaultValue="us">
+          <option value="us">US</option>
+        </select>
+      </form>
       <Suspense fallback="Loading...">
         <UserList />
       </Suspense>
