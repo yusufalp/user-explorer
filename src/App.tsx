f7581ca591688bfdf4d2b5246306450da2adc1b5
yusufalp <alpyusuf007@gmail.com>
2025-04-30 16:26:39 -0700

    compile and test un with default values

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import UserList from "./UserList";
+
 function App() {
   return (
     <>
       <h1>Users</h1>
+
+      <UserList />
     </>
   );
 }
--- src/UserList.tsx
+++ src/UserList.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { graphql, useLazyLoadQuery } from "react-relay";
 
+import { UserListQuery } from "./__generated__/UserListQuery.graphql";
+
 const UserList: React.FC = () => {
-  const data = useLazyLoadQuery(
+  const data = useLazyLoadQuery<UserListQuery>(
     graphql`
       query UserListQuery($results: Int, $nat: String) {
         users(results: $results, nat: $nat) {
@@ -21,13 +23,16 @@ const UserList: React.FC = () => {
         }
       }
     `,
-    {}
+    {
+      results: 200,
+      nat: "US",
+    }
   );
 
   return (
     <ul>
       {data?.users?.map((user) => (
-        <li key={user.login.uuid}>
+        <li key={user?.login?.uuid}>
           {user?.name?.first} {user?.name?.last}
         </li>
       ))}
