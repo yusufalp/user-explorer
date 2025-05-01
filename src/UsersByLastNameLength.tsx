1500fbe061b92efe8875459ab2d0033b638ca9c5
yusufalp <alpyusuf007@gmail.com>
2025-04-30 19:22:58 -0700

    add component to render user by last name length

---
+++ src/UsersByLastNameLength.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { User } from "./types/User";
+import { calculateCountByLastNameLength } from "./utils/calculateCountByLastNameLength";
+
+type Props = {
+  users: User[];
+};
+
+const UsersByLastNameLength: React.FC<Props> = ({ users }) => {
+  const lastNameCounts = calculateCountByLastNameLength(users);
+
+  return (
+    <div>
+      <h2>Users by Last Name Length</h2>
+      <ul style={{ listStyle: "none", padding: 0 }}>
+        {Object.entries(lastNameCounts).map(([length, count]) => (
+          <li key={length}>
+            <strong>{length}-letter</strong> last name: {count} {count === 1 ? "user" : "users"}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UsersByLastNameLength;
